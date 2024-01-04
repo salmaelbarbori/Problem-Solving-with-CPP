b5d09d7f81f6ba53cc50e5b592d524a6073788d7
SALMA EL BARBORI <73172522+salmaelbarbori@users.noreply.github.com>
2024-01-04 21:59:36 +0100

    rectified solution for 704. Binary Search.cpp

--- leetcode/704. Binary Search.cpp
+++ leetcode/704. Binary Search.cpp
@@ -3,30 +3,22 @@ public:
     int search(vector<int>& nums, int target) {
         int l, r, m;
         l = 0;
-        r = nums.size() - 1;
-        m = l - (l - r)/2;
-
-        if (r < 10)
-        {
-           for (auto i = 0; i <= r;i++)
-            if (nums[i] == target)
-                return i;
-           return -1;
-        }    
+        r = nums.size() - 1;  
 
         while (l < r) {
-            if (target > nums[m])
+            m = l - (l - r)/2;
+            if (target == nums[m])
+                return m;
+            else if (target >= nums[m])
             {
                 l = m + 1;
                 m = l - (l - r)/2;
             }
-            else if (target < nums[m])
+            else 
             {
                 r = m - 1;
                 m = l - (l - r)/2;
-            }
-            else
-                return m;
+            }           
         }
         return -1;
     }
