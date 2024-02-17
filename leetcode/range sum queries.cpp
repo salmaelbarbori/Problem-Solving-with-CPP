b498f3cf653baec68a4881f8ee87fa014acde0bb
SALMA EL BARBORI <73172522+salmaelbarbori@users.noreply.github.com>
2024-02-17 23:10:19 +0100

    Solution range sum queries.cpp

---
+++ leetcode/range sum queries.cpp
@@ -0,0 +1,23 @@
+class NumArray {
+public:
+    vector<int> temp;
+    NumArray(vector<int>& nums) {
+        for (auto i = 0; i < nums.size();i++)
+            temp.push_back(nums[i]);
+    }
+    
+    int sumRange(int left, int right) {
+        int sum = 0;
+        for(auto i = left;i<=right;i++)
+        {
+            sum+=temp[i];
+        }
+        return sum;
+    }
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray* obj = new NumArray(nums);
+ * int param_1 = obj->sumRange(left,right);
+ */
