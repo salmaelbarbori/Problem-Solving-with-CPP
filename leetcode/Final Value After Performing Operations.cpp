0f395d4a389e41ddf850e9c065790943c1ad7111
SALMA EL BARBORI <73172522+salmaelbarbori@users.noreply.github.com>
2024-02-29 23:15:40 +0100

    Solution Final Value After Performing Operations.cpp

---
+++ leetcode/Final Value After Performing Operations.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int x = 0;
+        for (auto i = 0;i < operations.size(); i++)
+        {
+            if (operations[i] == "++X" || operations[i] == "X++")
+                x = x + 1;
+            else
+                x = x - 1;
+        }
+        return x;
+    }
+};
