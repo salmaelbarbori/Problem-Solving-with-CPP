bc7cff110aa851e70aa678c66ac606940cb1c195
SALMA EL BARBORI <73172522+salmaelbarbori@users.noreply.github.com>
2024-04-29 22:20:49 +0100

    Create numberOfEmployeesWhoMetTarget.cpp

---
+++ leetcode/numberOfEmployeesWhoMetTarget.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int numberOfEmployeesWhoMetTarget(vector<int>& hours, int target) {
+        int cnt = 0;
+        for (auto i:hours)
+            if(i>=target)
+                cnt++;
+        return cnt;
+    }
+};
