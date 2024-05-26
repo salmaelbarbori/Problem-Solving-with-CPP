f54886f4770402407cc2c7d25a0e7aead1b8c4ed
SALMA EL BARBORI <73172522+salmaelbarbori@users.noreply.github.com>
2024-05-26 22:46:12 +0100

    Create 1491. Average Salary Excluding the Minimum and Maximum Salary.cpp

---
+++ leetcode/1491. Average Salary Excluding the Minimum and Maximum Salary.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    double average(vector<int>& salary) {
+        double ans = 0;
+        sort(salary.begin(), salary.end());
+        for(auto i = 1; i < salary.size() -1; ++i)
+            ans += salary[i];
+        return (ans/(salary.size()-2));
+    }
+};
