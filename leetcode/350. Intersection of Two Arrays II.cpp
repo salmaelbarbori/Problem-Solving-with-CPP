464011c2385f4a4e3ba578df042f0ae2d51d05da
SALMA EL BARBORI <73172522+salmaelbarbori@users.noreply.github.com>
2024-07-02 22:34:19 +0100

    Create 350. Intersection of Two Arrays II.cpp

---
+++ leetcode/350. Intersection of Two Arrays II.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        if ((nums1.find(nums2) != nspos:nums1.end()) || 
+        (nums2.find(nums1) != nspos:nums2.end())
+            return true;
+        return false;
+    }
+};
