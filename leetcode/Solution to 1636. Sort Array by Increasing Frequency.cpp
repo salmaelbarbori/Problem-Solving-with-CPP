e2a1b0c9d6d42148c79c1a0aabae5a412cde3925
SALMA EL BARBORI <73172522+salmaelbarbori@users.noreply.github.com>
2024-07-23 22:54:43 +0100

    Create Solution to 1636. Sort Array by Increasing Frequency.cpp

---
+++ leetcode/Solution to 1636. Sort Array by Increasing Frequency.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        map<int, int> m;
+        for (auto i = 0; i < nums.size(); ++i)
+           m[nums[i]]++;
+        sort(nums.begin(), nums.end(), [&](int a, int b) {
+            if (m[a] == m[b])
+                return a > b;
+            return m[a] < m[b];          
+        });
+        return nums;
+    }
+};
